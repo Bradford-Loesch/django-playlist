272f72d54a61925aec335ef91c242c1de78eb97f
Bradford Loesch <bradford.loesch@dialogtech.com>
2017-12-23 15:16:15 -0600

    more stuff

--- playlist/apps/playerapp/models.py
+++ playlist/apps/playerapp/models.py
@@ -7,13 +7,46 @@ import re
 
 class ObjectManager(models.Manager):
     def valid_login(self, data):
-        pass
+        Users.objects.get(email = data.email)
     def valid_register(self, data):
-        pass
+        email_re = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        errors = []
+        if len(data.first_name) < 2:
+            errors.append("First name must be at least two characters")
+        if not data.first_name.isalpha():
+            errors.append("First name must be letters")
+        if len(data.last_name) < 2:
+            errors.append("Last name must be at least two characters")
+        if not data.last_name.isalpha():
+            errors.append("Last name must be letters")
+        if not email_re.match(data.email):
+            errors.append("Must be a valid email")
+        if not data.passworda == passwordb:
+            errors.append("Passwords must match")
+        if len(data.passworda) < 8:
+            errors.append("Password must be at least 8 characters")
+        if data.bday == 1:
+            pass
+        if len(errors) > 0:
+            return False, errors
+        else:
+            hashed = encrypt(data.passworda)
+            User.objects.create(first_name = data.first_name, last_name = data.last_name, email = data.email, password = hashed, birthday = data.bday)
+            return True, 
     def valid_song(self, data):
         pass
     def valid_artist(self, data):
         pass
+    def encrypt(password):
+        password = password.encode()
+        hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+        return hashed
+    def comparePass(password, hashed):
+        password = password.encode()
+        hashed = hashed.encode()
+        if bcrypt.hashpw(password, hashed) == hashed:
+            return True
+        return False
 
 
 # Create your models here.
--- playlist/apps/playerapp/views.py
+++ playlist/apps/playerapp/views.py
@@ -2,20 +2,24 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
+import models
 
 # Create your views here.
 def index(request):
     return render(request, 'playerapp/index.html')
 
 def login(request):
-    if 'user' in session:
+    result = User.objects.valid_login(request.POST)
+    if 'user' in request.session:
         return redirect('/dashboard')
     else:
         return redirect('/')
 
 def register(request):
-    print request.POST
-    if 'user' in session:
+    print request.POST['bday']
+    result = Users.objects.valid_register(request.POST)
+    print result
+    if result[0]:
         return redirect('/dashboard')
     else:
         return redirect('/')
